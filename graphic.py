c854c7bd5771da8907510c5e763c98a1e494ddf0
boury_a <boury_a@etna-alternance.net>
2021-07-19 00:10:56 +0200

    second commit

--- graphic.py
+++ graphic.py
@@ -1,24 +1,48 @@
 import tkinter as tk
-import tkinter.font
-#from file_generator import *
+from tkinter import font
 
 
 class app_window:
-    def __init__(self, title: str, font: str, window_size: tuple) -> None:
+    def __init__(self, title: str, *window_size: tuple) -> None:
         self.root = tk.Tk()
-        self.font = tkinter.font.Font(family=font)
+        print(font.families())
+        if not window_size:
+            w = 800  # width for the Tk root
+            h = 650  # height for the Tk root
+        else:
+            w = window_size[0][0]
+            h = window_size[0][1]
+        # get screen width and height
+        ws = self.root.winfo_screenwidth()  # width of the screen
+        hs = self.root.winfo_screenheight()  # height of the screen
+
+        # calculate x and y coordinates for the Tk root window
+        x = (ws/2) - (w/2)
+        y = (hs/2) - (h/2)
+
+        # set the dimensions of the screen
+        # and where it is placed
+        self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+        # self.font = tkinter.font.Font(family=font)
+        self.defaultFont = font.nametofont("TkDefaultFont")
+        self.defaultFont.configure(family="Javanese Text",
+                                   size=10,
+                                   weight=font.NORMAL)
         self.root.title(title)
-        self.root.geometry(str(window_size[0])+'x'+str(window_size[1]))
 
     def menu(self):
         label = tk.Label(
-            self.root, text=" Welcolme to Spreadsheet's Revenge. Clean up your data like a noob!")
-        label.pack()
+            self.root, text=" Welcome to Spreadsheet Cleaner.\n Choose an action:")
+        label.grid(row=0, column=1)
+        data_generation_button = tk.Button(text="Data generation menu", width=20,
+                                           height=2)
+        data_generation_button.grid(row=1, column=0, padx=10, pady=30)
 
     def main(self):
         self.root.mainloop()
 
 
-menu_window = app_window("xfileFormatter", "Helvetica", (600, 400))
+menu_window = app_window("Spreadsheet Cleaner", (600, 400))
 menu_window.menu()
 menu_window.main()
