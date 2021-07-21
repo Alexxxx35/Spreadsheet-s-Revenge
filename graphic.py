649f6f9f31b6ab97006d5bb160a0c40b6a200aba
boury_a <boury_a@etna-alternance.net>
2021-07-21 11:03:15 +0200

    callback implementation

--- graphic.py
+++ graphic.py
@@ -1,22 +1,24 @@
 import tkinter as tk
-from tkinter import font, Menu
+from tkinter import StringVar, font, Menu
 import os
 
 
 class app_window:
     def __init__(self, title: str, *window_size: tuple) -> None:
         self.root = tk.Tk()
-        print(font.families())
+       # print(font.families())
         self.root.iconbitmap(os.path.join("images", "spreadsheet.ico"))
-        #self.menubar = Menu(self.root, background="blue", fg="grey")
+
+        # self.menubar = Menu(self.root, background="blue", fg="grey")
         # self.root.config(menu=self.menubar)
+
         if not window_size:
             w = 800  # width for the Tk root
             h = 650  # height for the Tk root
         else:
             w = window_size[0][0]
             h = window_size[0][1]
-        # get screen width and height
+
         ws = self.root.winfo_screenwidth()  # width of the screen
         hs = self.root.winfo_screenheight()  # height of the screen
 
@@ -24,11 +26,9 @@ class app_window:
         x = (ws/2) - (w/2)
         y = (hs/2) - (h/2)
 
-        # set the dimensions of the screen
-        # and where it is placed
+        # set the dimensions of the screen and where it is placed
         self.root.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
-        # self.font = tkinter.font.Font(family=font)
         self.defaultFont = font.nametofont("TkDefaultFont")
         self.defaultFont.configure(family="Javanese Text",
                                    size=10,
@@ -46,12 +46,31 @@ class app_window:
     def main(self):
         self.root.mainloop()
 
+    def entry_clicker(self, event):
+        print(self.number_of_rows_value.get())
+
     def data_generation_page(self):
         self.menu_label.destroy()
         self.data_generation_button.destroy()
         self.generator_menu_label = tk.Label(
             self.root, text="Spreadsheet Cleaner File Generator")
-        self.generator_menu_label.grid(row=0, column=1)
+        self.generator_menu_label.place(x=180, y=1)
+
+        entries = []
+        self.number_of_row_label = tk.Label(
+            self.root, text="how many fields must include the false document:")
+        self.number_of_row_label.grid(row=1, column=0, pady=50)
+        self.number_of_rows_value = StringVar()
+        self.number_of_rows = tk.Entry(
+            self.root, textvariable=self.number_of_rows_value)
+        self.number_of_rows.grid(row=1, column=1, sticky="w")
+        self.number_of_rows.bind("<Return>", self.entry_clicker)
+        
+        # for x in range(20):
+        #     entry = "entry{}".format(x)
+        #     entries.append(entry)
+        # for str in entries:
+        # str = tk.Entry(self.root,)
 
 
 menu_window = app_window("Spreadsheet Cleaner", (600, 400))
